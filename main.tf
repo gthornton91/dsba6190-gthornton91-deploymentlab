beeb0a1a136359a1f7595ec97b074d19588fede4
gthornton91 <110245288+gthornton91@users.noreply.github.com>
2024-11-13 20:26:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,24 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virtual network
+
+resource "azurerm_virtual_network" "virtual" {
+  name                = "vn${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// Subnet
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.virtual.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 // Storage Account
 
@@ -39,6 +57,42 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  is_hns_enabled           = true
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
 
   tags = local.tags
+}
+
+// SQL stuff
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sql-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name         = "db-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "Basic"
+
+  tags = {
+    foo = "bar"
+  }
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sql-vnetrule" {
+  name      = "sql-vnet${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id = azurerm_mssql_server.sql.id
+  subnet_id = azurerm_subnet.subnet.id
 }
